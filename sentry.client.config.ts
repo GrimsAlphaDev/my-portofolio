141318434aa20618b03fe4afc9737527deed0452
GrimsAlphaDev <kungkingkangcanyouseemekuking@gmail.com>
2024-09-25 22:17:57 +0700

    logging on sentry

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -29,4 +29,6 @@ Sentry.init({
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+  
 });
+console.log("Sentry initialized on the client");
